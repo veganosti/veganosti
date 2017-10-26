2c38267af6189013416a78436da464f821d96982
Daniel Santamaría <santaka87@gmail.com>
2017-10-26 19:43:43 +0200

    semantic ui setup. Places list with semantic ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import PlaceList from './components/PlaceList'
+import PlaceList from './components/PlaceList';
+import places from './data/places';
 
 class App extends Component {
   state = {
-    places: [
-      {"name": "Landare", "address": "Trueba Kalea, 4"},
-      {"name": "Garraxi", "address": "Tejeria kalea, 9"},
-    ]
+    places
   }
 
   render() {
     return (
       <div>
-        <h1>DBegano</h1>
+        <h1>DBegano | Veganosti | Beganosti</h1>
         <PlaceList places={this.state.places} />
       </div>
     );
