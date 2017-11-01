bfab23ea00ae8562f7a982a1d2747682c3bea4b2
Daniel Santamaría <santaka87@gmail.com>
2017-11-01 10:12:34 +0100

    use env variable for google maps API

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -5,7 +5,7 @@ import PlaceFilter from './PlaceFilter'
 const MapPage = () => (
   <div>
     <Map
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,drawing,places`}
       containerElement={<div id="map" style={{height: `93vh`}}/>}
       loadingElement={<div style={{height: `100%`}}/>}
       mapElement={<div style={{height: `100%`}}/>}
