bb03285faef5a8b808bf4e2c29534095de455ef5
Daniel Santamaría <santaka87@gmail.com>
2017-11-05 18:29:54 +0100

    customize top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter, matchPath } from 'react-router'
 import { Link } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
@@ -8,6 +9,7 @@ import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import MapIcon from 'material-ui-icons/Map';
 import ListIcon from 'material-ui-icons/List';
+import BackIcon from 'material-ui-icons/KeyboardBackspace';
 
 const styles = () => ({
   root: {
@@ -18,20 +20,36 @@ const styles = () => ({
   }
 });
 
-function TopBar({classes}) {
+function TopBar({classes, location, history}) {
+
+  const isDetailPage = matchPath(location.pathname, {
+    path: '/places/:id',
+    exact: true,
+  })
+
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography type="title" color="inherit" className={classes.flex}>
-            Veganosti
-          </Typography>
-          <IconButton color="contrast" component={Link} to="/">
-            <MapIcon/>
-          </IconButton>
-          <IconButton color="contrast" component={Link} to="/places">
-            <ListIcon/>
-          </IconButton>
+          {isDetailPage ? (
+            <IconButton color="contrast" onClick={history.goBack}>
+              <BackIcon/>
+            </IconButton>
+          ) : (
+            <Typography type="title" color="inherit" className={classes.flex}>
+              Veganosti
+            </Typography>
+          )}
+          {location.pathname === '/places' && (
+            <IconButton color="contrast" component={Link} to="/">
+              <MapIcon/>
+            </IconButton>
+          )}
+          {location.pathname === '/' && (
+            <IconButton color="contrast" component={Link} to="/places">
+              <ListIcon/>
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
     </div>
@@ -43,6 +61,12 @@ TopBar.propTypes = {
     root: PropTypes.string.isRequired,
     flex: PropTypes.string.isRequired,
   }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired
 };
 
-export default withStyles(styles)(TopBar);
+export default withStyles(styles)(withRouter(TopBar));
